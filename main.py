fdd6965bc883ba340a04a9b1141b28e3a519ef54
Vihaan Kembhavi <vihaankembhavi12@gmail.com>
2024-07-11 15:47:29 -0700

    feat: Ask whether user is organizer or attendee. Possibility to edit current event.

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 # Parameters: location, date, time, parking, meal times.
 from os import path
-from json import dump
+from json import dump, load
 from chatbot import openai, get_response
 
-if not path.exists('.json'):
+def get_info():
+    global event_info
     event_info={}
-    end_of_input='If it doesn\'t exist, then just hit enter.\n\n'
+    end_of_input='If it doesn\'t exist, then just press enter.\n'
 
     location=input(f'What is the location of the event? {end_of_input}')
     date=input(f'\nWhat are the days that the event is taking place? {end_of_input}')
@@ -20,24 +21,58 @@ if not path.exists('.json'):
     event_info['meal timings']=meal_timings
 
     for key in event_info:
-        if event_info[key] == '':
+        if event_info[key]=='':
             event_info[key]=None
-
-    print(event_info)
-
+    
     with open('.json', 'w') as JSON:
         dump(event_info, JSON)
 
-print('Hello, I will be your AI assistant today.')
-print('Feel free to ask me any questions about the event that you\'re attending.\n')
+def get_details():
+    print('Here are the details:')
+    for key in event_info:
+        print(f'{key}: {event_info[key]}')
+
+
 while True:
-    with open('.json', 'r') as JSON:
-        text = ' '.join(line.rstrip() for line in JSON)
-    user_input=input('User: ')
-    if user_input.lower() in ['exit', 'quit']: print('Bye'); break
-    refined_input=f'''{user_input}\n\nRefer to this .json file:\n\n{text}\n
-    Answer me as if I was someone attending the event. 
-    Only give the answer to the question that was asked. Don't include any other parameters.'''
-    response=get_response(refined_input)
-    print('Chatbot:',response)
-    print('')
+    role=input('Are you an event organizer or an attendee? Answer with attendee/organizer.\n')
+    print()
+    match role.lower():
+        case 'organizer':
+            if not path.exists('.json'):
+                get_info()
+                print('Thanks for the info. Your event has been organized.\n')
+                get_details()
+
+                break
+            else:
+                with open ('.json', 'r') as JSON:
+                    event_info = load(JSON)
+                print('Good, your event is already organized.\n')
+                get_details()
+                change='change'
+                while change.lower()=='change':
+                    change=input('\nIf you want to change the details, type \'change\'. Otherwise press enter.\n')
+                    if change.lower()=='change':
+                        get_info()
+                        print('Thanks for the info. Your event has been updated.')
+                        get_details()
+                break
+
+        case 'attendee':
+            print('Hello, I will be your AI assistant today.')
+            print('Feel free to ask me any questions about the event that you\'re attending.\n')
+            while True:
+                with open('.json', 'r') as JSON:
+                    text = ' '.join(line.rstrip() for line in JSON)
+                user_input=input('User: ')
+                if user_input.lower() in ['exit', 'quit']: 
+                    print('Bye')
+                    quit()
+                refined_prompt=f'''{user_input}\n\nRefer to this .json file:\n\n{text}\n
+                Answer me as if I was someone attending the event. 
+                Only give the answer to the question that was asked. Don't include any other parameters.'''
+                response=get_response(refined_prompt)
+                print('Chatbot:',response)
+                print()
+        case _:
+            print('Please answer with either \'attendee\' or \'organizer\'.\n')
