8ad3d1b719f9e0705c1cc3de84419b1af411e62b
Vihaan Kembhavi <vihaankembhavi12@gmail.com>
2024-07-18 22:18:02 -0700

    Refined prompt 2 and deleted prompt 1. For optional events, N/A can be entered

--- new.py
+++ new.py
@@ -11,7 +11,7 @@ def get_response(prompt):
     response=api.chat.completions.create(
         messages=[*conversation_history,{'role':'user','content':prompt}],
         model='gpt-4o',
-        temperature=0.2
+        temperature=0.3
     )
     return response.choices[0].message.content.strip()
 
@@ -34,39 +34,11 @@ def iterate_questions(questions):
         print('Sorry, something went wrong, please try again.')
         quit()
 
-prompt='''
-We are organizing an event and we would like to get the following details from the organizer:
-    - Mandatory details: event name, event description, location, date
-    - Optional details: parking location, food options, seating
-
-The questions should meet the following criteria:
-    1. The questions should be short and concise
-    2. The questions should be grammatically correct i.e. include capital letter and question mark
-    3. The questions should indicate that the optional details can be skipped by entering 'Skip'
-    4. For each detail there should be 1 question i.e. no combining details into 1 question 
-       or separating details into multiple questions.
-    5. The questions should be explicit e.g. If the question is about date, 
-       ask 'What date is the event happening?' instead of asking 'When is the event?'
-    6. There should be a space after the question i.e. '...? ' instead of '...?'
-    7. There shouldn't be any duplicate question. Include each question only once
-
-Provide the list of questions back to me in a JSON array in vector form. Avoid nested objects.
-Avoid using '```json```' or any other text besides the JSON array. 
-Make sure the response is a valid JSON list with no parsing errors.
-
-Example of a valid JSON list response:
-[
-    "What do you want to name the event? ", 
-    "Where is parking available? (Enter Skip if not applicable) "
-]
-'''
-
-response=get_response(prompt)
-event_questions=iterate_questions(json.loads(response))
+event_questions={}
 event={}
 
 while True:
-    prompt2=f'''
+    prompt=f'''
 We are organizing an event and we would like to get the following details from the organizer:
     - Mandatory details: event name, event description, location, date
     - Optional details: parking, food options, seating
@@ -75,7 +47,7 @@ The following questions and answers are already available in JSON list format:
 {json.dumps(event_questions, indent=4)}
 
 Make sure that all mandatory details are filled in and not empty.
-It's okay if the optional details aren't present.
+It's okay if the optional details are empty.
 
 The following 'event' JSON object is already built:
 {json.dumps(event, indent=4)}
@@ -84,21 +56,30 @@ Update the above 'event' JSON object with all the details populated from the ans
 It doesn't matter if the optional fields are empty.
 
 The object should meet the following criteria:
-    1. The keys should be the specified parameters: 'event name', 'event description', ... (The ones mentioned earlier).
+    1. The keys should be the specified parameters: 'event name', 'event description', ... (The ones mentioned above).
     2. Avoid nested objects.
-    3. If certain optional details aren't provided, the value should be null.
-    4. A mandatory detail (date, location, etc) may not have the value null.
+    3. Each parameter may be present only once i.e. no duplicates.
     
-    e.g. {'{"date":"8 September 2024","time":"4PM - 7PM"}'}
+    e.g. {'{"date":"8 September 2024", "event name":"Bob\'s Birthday party"}'}
 
-If not all information from the mandatory fields is present or complete, build a 'questions' JSON array 
-in vector form of all questions where extra details must be provided. 
-Don't ask me questions if the details are already complete.
+If for some event details the value is an empty string, build a 'questions' JSON array 
+in vector form of questions for those details. Don't ask me questions if the details are already present.
+If a detail was given, adding the information is mandatory.
+
+The questions should meet the following criteria:
+    1. The questions should be short and concise
+    2. The questions should be grammatically correct i.e. include capital letter and question mark
+    3. The questions should indicate that the optional details can be left empty by entering 'N/A'.
+    4. For each detail there should be 1 question i.e. no combining details into 1 question 
+       or separating details into multiple questions.
+    5. The questions should be explicit e.g. If the question is about date, 
+       ask 'What date is the event happening?' instead of asking 'When is the event?'.
+    6. There should be a space after the question i.e. '...? ' instead of '...?'.
+    7. There shouldn't be any duplicate question. Include each question only once.
 
 The array should meet the following criteria:
     1. The array must be in vector form. Avoid nested arrays.
-    3. Don't include questions for optional details e.g. parking, seating, etc.
-    4. Don't include ```json``` or any other text besides the JSON array. 
+    2. Don't include ```json``` or any other text besides the JSON array. 
        Make sure the response is a valid JSON array with no parsing errors.
 
 Return a JSON object containing the 'event' JSON object and the 'questions' JSON array.
@@ -111,24 +92,24 @@ Example:
     "description": "abc",
     "location": "",
     "date": "",
-    "parking": null,
-    "food options": null,
+    "parking": "",
+    "food options": "",
     "seating": "seat 3A"
   },
   "questions": [
     "What is the location of the event? ",
-    "What date is the event happening? "
+    "What date is the event happening? ",
+    "What food options are available? "
   ]
 }'''}    
 '''
     
-    response=get_response(prompt2)
-    print(response)
+    response=get_response(prompt)
     response_object=json.loads(response)
     event=response_object['event']
 
     if len(response_object.get('questions', [])) >0: 
-        print('\nPlease include more details for the following questions:')
+        print('\nPlease include the details for the following questions:')
         event_questions=iterate_questions(response_object['questions'])
         continue
     break
