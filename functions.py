d7f57573d247d30f2b6001a7574ce267ceca0a9e
Vihaan Kembhavi <vihaankembhavi12@gmail.com>
2024-07-23 21:40:08 -0700

    Made code easier to read in functions.py file

--- functions.py
+++ functions.py
@@ -7,51 +7,52 @@ from bson.objectid import ObjectId
 from bson.errors import InvalidId
 
 load_dotenv('.env')
-api:str=OpenAI(api_key=getenv('API_KEY'))
-conversation_history=[]
+api:str=OpenAI(api_key = getenv('API_KEY'))
+conversation_history = []
 
 def get_response(prompt):
-    response=api.chat.completions.create(
-        messages=[*conversation_history,{'role':'user','content':prompt}],
-        model='gpt-4o',
-        temperature=0.3
+    response = api.chat.completions.create(
+        messages = [*conversation_history, {'role':'user','content':prompt}],
+        model = 'gpt-4o',
+        temperature = 0.3
     )
     return response.choices[0].message.content.strip()
 
 def get_json_object_response(prompt):
-    response=api.chat.completions.create(
-        messages=[*conversation_history,{'role':'user','content':prompt}],
-        model='gpt-4o',
-        temperature=0.3,
-        response_format={'type':'json_object'}
+    response = api.chat.completions.create(
+        messages = [*conversation_history,{'role':'user','content':prompt}],
+        model = 'gpt-4o',
+        temperature = 0.3,
+        response_format = {'type':'json_object'}
     )
     return response.choices[0].message.content.strip()
 
 def store_message(user_input, response):
     conversation_history.extend([
-        {'role':'user','content':user_input},{'role':'system','content':response}
+        {'role':'user', 'content':user_input},{'role':'system', 'content':response}
     ])
 
-client=MongoClient('mongodb://localhost:27017')
-db=client['mydb']
-events=db['events']
+client = MongoClient('mongodb://localhost:27017')
+db = client['mydb']
+events = db['events']
 
 def iterate_questions(questions):
-    event_questions=[]
+    event_questions = []
     try:
         for question in questions:
-            ans=input(question)
-            if ans.lower()=='skip':ans=None
+            ans = input(question)
+            if ans.lower() == 'skip':
+                ans = None
             event_questions.append({'question':question,'answer':ans})
         return event_questions
     except json.decoder.JSONDecodeError:
         print('Sorry, something went wrong, please try again.')
 
 def create_event():
-    answered_questions={}
-    event={}
+    answered_questions = {}
+    event = {}
     while True:
-        prompt=f'''
+        prompt = f'''
 We are organizing an event and we would like to get the following details from the organizer:
     - Mandatory details: event name, event description, location, date, start time, end time, contact info of organizer
     - Optional details: parking, food options, seating, wifi info of venue, schedule of event 
@@ -121,39 +122,45 @@ Example:
 }'''}    
 '''
         
-        response=get_json_object_response(prompt)
-        response=response[response.index('{'):response.rfind('}')+1]
+        response = get_json_object_response(prompt)
+        response = response[response.index('{'):response.rfind('}')+1]
+
         try:
-            response_object=json.loads(response)
+            response_object = json.loads(response)
         except json.decoder.JSONDecodeError:
             return 'Something went wrong, please try again'
         store_message(prompt, response)
+        
         if response_object.get('event'):
-            event=response_object.get('event')
+            event = response_object.get('event')
 
-        if len(response_object.get('questions', [])) >0: 
+        if len(response_object.get('questions', [])) > 0: 
             print('\nPlease include the details for the following questions:')
-            answered_questions=iterate_questions(response_object['questions'])
+            answered_questions = iterate_questions(response_object['questions'])
             continue
         break
 
-    id=events.insert_one(event)
+    id = events.insert_one(event)
     return f'''\nEvent was succesfully created. Here is the event id: {id.inserted_id}. Store 
 it somewhere, because you\'ll need it to edit or delete your event.'''
 
 def delete_event(event_id):
     try:
-        result=events.delete_one({'_id':ObjectId(event_id)})
+        result = events.delete_one({'_id':ObjectId(event_id)})
     except InvalidId:
         return 'Sorry, invalid id'
-    if result.deleted_count: return 'The event was succesfully deleted.'
+    
+    if result.deleted_count: 
+        return 'The event was succesfully deleted.'
+    
     return 'Sorry, no event with that id found.'
 
 def chat(event_id):
     try:
-        event_id=ObjectId(event_id)
+        event_id = ObjectId(event_id)
     except InvalidId:
         return 'Sorry invalid id.'
+    
     print('Hello, I will be your AI assistant today.')
     print('Feel free to ask me any questions about the event that you\'re attending.')
     print('If you want to stop talking to me, just enter \'exit\' or \'quit\'.\n')
@@ -161,7 +168,7 @@ def chat(event_id):
     if not [doc for doc in events.find({'_id':event_id})]:
         return 'Sorry no event with that id found'
     
-    prompt=f'''
+    prompt = f'''
 Here is the event info:
 {[doc for doc in events.find({'_id':ObjectId(event_id)})][0]}
 Give me information about this event
@@ -169,39 +176,48 @@ Give me information about this event
     conversation_history.append({'role':'user','content':prompt})
     
     while True:
-        user_input=input('User: ')
+        user_input = input('User: ')
         if user_input in ['exit','quit']:
             return 'Thanks for talking to me!. If you have anymore questions, don\'t hesitate to ask!'
 
-        refined_input=f'''Act as an event organizer and provide an 
+        refined_input = f'''Act as an event organizer and provide an 
                     answer to the question. Refer to the MongoDB collection provided.
                     Give a short and concise answer. Only give an answer to the
                     question that was asked. Don't include any unnecessary details.
                     
                     {user_input}'''
 
-        response=get_response(refined_input)
+        response = get_response(refined_input)
         store_message(refined_input, response)
         print('Chatbot:',response)
 
 def get_details(event_id):
     try:
-        event_id=ObjectId(event_id)
+        event_id = ObjectId(event_id)
     except InvalidId:
         return 'Sorry, invalid id.'
-    if not [doc for doc in events.find({'_id': event_id})]: return 'This event doesn\'t exist'
-    event_info=[doc for doc in events.find({'_id':event_id})][0]
+    
+    if not [doc for doc in events.find({'_id': event_id})]: 
+        return 'This event doesn\'t exist'
+    
+    event_info = [doc for doc in events.find({'_id':event_id})][0]
+
     print('\nHere are the event details for your event:')
     for key in event_info:
-        if key!='_id':print(f'{key}: {event_info[key]}')
+        if key != '_id':
+            print(f'{key}: {event_info[key]}')
+
     return ''
 
 def change_event(event_id, wrong_details):
     try:
-        event_id=ObjectId(event_id)
+        event_id = ObjectId(event_id)
     except InvalidId:
         return 'Sorry invalid id.'
-    if not [doc for doc in events.find({'_id':event_id})][0]: return 'Sorry event not found.'
+    
+    if not [doc for doc in events.find({'_id':event_id})][0]: 
+        return 'Sorry event not found.'
+    
     prompt=f'''
 The organizer of an event has made a mistake while entering a detail for this event.
 
@@ -233,17 +249,19 @@ then your response should be:
 Avoid nested objects, and make all the keys and values strings only.
 '''
 
-    response=get_json_object_response(prompt)
-    response=response[response.index('{'):response.rfind('}')+1]
+    response = get_json_object_response(prompt)
+    response = response[response.index('{') : response.rfind('}')+1]
     try:
-        response=json.loads(response)
+        response = json.loads(response)
     except json.decoder.JSONDecodeError:
         return 'Sorry, something went wrong, please try again'
     
-    list_keys=['event name','event description','location','date','start time', 'end time', 'food options',
+    list_keys = ['event name','event description','location','date','start time', 'end time', 'food options',
     'contact info of organizer','parking', 'seating','wifi info of venue', 'schedule of event']
+    
     for key in response:
-        if key.strip() not in list_keys: return 'Sorry, something went wrong, please try again.'
+        if key.strip() not in list_keys: 
+            return 'Sorry, something went wrong, please try again.'
     
     events.update_many({'_id':ObjectId(event_id)}, {'$set':response})
     
